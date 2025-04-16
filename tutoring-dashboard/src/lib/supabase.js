87f562d8f37427f9f6ae1ce547b9397047c3a68f
Maro <55569476+RDDcat@users.noreply.github.com>
2025-04-16 23:15:50 +0900

    배포 환경변수  셋팅

--- tutoring-dashboard/src/lib/supabase.js
+++ tutoring-dashboard/src/lib/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://dtnthpwvvgsvapbfjcmq.supabase.co' // 본인 프로젝트 URL
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0bnRocHd2dmdzdmFwYmZqY21xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2MzQ3MzUsImV4cCI6MjA2MDIxMDczNX0.kHA-3N_UXEOJMX2gEPMoN9r8LKjnof4MY-yneg9hfuw' // anon public key
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
