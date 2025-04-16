25d953d93e21401529c2af564b951f245ad54973
Maro <55569476+RDDcat@users.noreply.github.com>
2025-04-16 22:58:22 +0900

    ui수정

--- tutoring-dashboard/src/layouts/DefaultLayout.vue
+++ tutoring-dashboard/src/layouts/DefaultLayout.vue
@@ -2,9 +2,7 @@
   <div class="relative flex min-h-screen bg-gray-50">
     <Sidebar />
 
-    <main
-      class="flex-1 px-4 sm:px-6 md:px-8 py-6 ml-0 md:ml-60 transition-all duration-300 ease-in-out"
-    >
+    <main class="flex-1 px-4 sm:px-6 md:px-8 py-6 transition-all duration-300 ease-in-out">
       <RouterView />
     </main>
   </div>
